449ce1176286476e68953f423ebc6fb4d03918e6
Sarah <sarah.schoenherr@gmx.de>
2017-01-24 22:06:47 +0100

    Updated data model.

--- database/create_database.sql
+++ database/create_database.sql
@@ -50,3 +50,51 @@ CREATE TABLE `buch_schlagwort` (
   CONSTRAINT `FK_Buch_Schlagwort_ID_Buch` FOREIGN KEY (`ID_Buch`) REFERENCES `buch` (`ID`) ON UPDATE CASCADE,
   CONSTRAINT `FK_Buch_Schlagwort_ID_Schlagwort` FOREIGN KEY (`ID_Schlagwort`) REFERENCES `schlagwort` (`ID`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sachgruppe` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Sachgruppe` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `index_sachgruppe` (`Sachgruppe`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `buch_sachgruppe` (
+  `ID_Buch` int(10) unsigned NOT NULL,
+  `ID_Sachgruppe` int(10) unsigned NOT NULL,
+  KEY `FK_Buch_Sachgruppe_ID_Buch` (`ID_Buch`),
+  KEY `FK_Buch_Sachgruppe_ID_Sachgruppe` (`ID_Sachgruppe`),
+  CONSTRAINT `FK_Buch_Sachgruppe_ID_Buch` FOREIGN KEY (`ID_Buch`) REFERENCES `buch` (`ID`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_Buch_Sachgruppe_ID_Sachgruppe` FOREIGN KEY (`ID_Sachgruppe`) REFERENCES `sachgruppe` (`ID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `gattung` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Gattung` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `index_gattung` (`Gattung`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `buch_gattung` (
+  `ID_Buch` int(10) unsigned NOT NULL,
+  `ID_Gattung` int(10) unsigned NOT NULL,
+  KEY `FK_Buch_Gattung_ID_Buch` (`ID_Buch`),
+  KEY `FK_Buch_Gattung_ID_Gattung` (`ID_Gattung`),
+  CONSTRAINT `FK_Buch_Gattung_ID_Buch` FOREIGN KEY (`ID_Buch`) REFERENCES `buch` (`ID`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_Buch_Gattung_ID_Gattung` FOREIGN KEY (`ID_Gattung`) REFERENCES `gattung` (`ID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `inhalt` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Inhalt` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `index_inhalt` (`Inhalt`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `buch_inhalt` (
+  `ID_Buch` int(10) unsigned NOT NULL,
+  `ID_Inhalt` int(10) unsigned NOT NULL,
+  KEY `FK_Buch_Inhalt_ID_Buch` (`ID_Buch`),
+  KEY `FK_Buch_Inhalt_ID_Inhalt` (`ID_Inhalt`),
+  CONSTRAINT `FK_Buch_Inhalt_ID_Buch` FOREIGN KEY (`ID_Buch`) REFERENCES `buch` (`ID`) ON UPDATE CASCADE,
+  CONSTRAINT `FK_Buch_Inhalt_ID_Inhalt` FOREIGN KEY (`ID_Inhalt`) REFERENCES `inhalt` (`ID`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
