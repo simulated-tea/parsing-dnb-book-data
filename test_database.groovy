25507ef5439e1dfa05ee7ba69c5c86e7829e42be
Simon <t-sim@gmx.net>
2017-03-05 16:25:45 +0100

    Once again an incomplete implementation of the db connector

--- test_database.groovy
+++ test_database.groovy
@@ -6,7 +6,7 @@ schema_config = [
     [
         table: 'book',
         columns: [
-            [name: "titel", type: "varchar(255)", source: "titel"],
+            [name: "title", type: "varchar(255)", source: "title"],
             [name: "isbn10", type: "int(10)", source: "isbn"],
         ],
     ], [
@@ -21,7 +21,8 @@ schema_config = [
             [name: "id_author", type: "int(10)"],
         ],
     ], [
-        table: 'tag',
+        table: 'tag', // Bad example, since usually tag would rather be done via a join table
+                      // like for author above.
         columns: [
             [name: "id_book", type: "int(10)"],
             [name: "name", type: "varchar(255)", source: "tags"],
@@ -31,17 +32,19 @@ schema_config = [
 
 config = (new ConfigSlurper()).parse(new URL('file:config.groovy'))
 config.database.url = config.database.url.replace('/books', '/test')
+
 config.database.schema_config = schema_config
 connector = new MariaDbConnector(config)
+connector.log_debug = false
 assert 'OK' == connector.testConnection()
 
-executeWithPresentTestTables{
+executeWithPresentTestTables{ // can insert book data
     assert connector.read('book') == []
     assert connector.read('author') == []
     assert connector.read('book_author') == []
     assert connector.read('tag') == []
 
-    connector.insertData([
+    connector.importBookData([
         [
             title: "the book",
             isbn: 1234567890,
@@ -51,14 +54,15 @@ executeWithPresentTestTables{
     ])
             
     assert connector.read('book') == [[id: 1, title: "the book", isbn10: 1234567890]]
-    assert connector.read('author') == [[id: 1, name: "the prophet"]]
+    assert connector.read('author') == [[ID: 1, name: "the prophet"]]
     assert connector.read('book_author') == [[id_book: 1, id_author: 1]]
     assert connector.read('tag') == [
-        [id: 1, id_book: 1, name: "clever"],
-        [id: 2, id_book: 1, name: "lies"],
-        [id: 3, id_book: 1, name: "fantasy"],
+        [ID: 1, id_book: 1, name: "clever"],
+        [ID: 2, id_book: 1, name: "lies"],
+        [ID: 3, id_book: 1, name: "fantasy"],
     ]
 }
+// TODO: Test with reusing existing author from db!
 
 println "Test successful!"
 
@@ -77,10 +81,7 @@ private executeWithPresentTestTables(Closure code) {
         code()
     } finally {
         try {
-            connector.sql.execute 'DROP TABLE book'
-            connector.sql.execute 'DROP TABLE author'
-            connector.sql.execute 'DROP TABLE book_author'
-            connector.sql.execute 'DROP TABLE tag'
+            dropTestTables()
         } catch (e) {
             println "Something went horribly wrong. Check the database for leftover test data!"
             System.exit 1
@@ -92,7 +93,7 @@ private createTestTables() {
     connector.sql.execute '''
         CREATE TABLE book (
             id INTEGER NOT NULL AUTO_INCREMENT KEY,
-            titel VARCHAR(255),
+            title VARCHAR(255),
             isbn10 INTEGER
         )
     '''
@@ -102,6 +103,7 @@ private createTestTables() {
             name VARCHAR(255)
         )
     '''
+    // TODO: add foreign keys
     connector.sql.execute '''
         CREATE TABLE book_author (
             id_book INTEGER,
@@ -115,4 +117,11 @@ private createTestTables() {
             name VARCHAR(255)
         )
     '''
+}
+
+private dropTestTables() {
+    connector.sql.execute 'DROP TABLE book'
+    connector.sql.execute 'DROP TABLE author'
+    connector.sql.execute 'DROP TABLE book_author'
+    connector.sql.execute 'DROP TABLE tag'
 }
--- util/MariaDbConnector.groovy
+++ util/MariaDbConnector.groovy
@@ -10,6 +10,9 @@ class MariaDbConnector {
     def db_config
     def datasource
     def schema_config
+    def log_debug = false
+
+    def recentlyInsertedCache = [:]
 
     MariaDbConnector(config) {
         config              = config instanceof Map ? config : (new ConfigSlurper()).parse(new URL('file:' + config))
@@ -17,8 +20,9 @@ class MariaDbConnector {
         datasource.user     = config.database.user
         datasource.password = config.database.password
         schema_config       = config.database.schema_config
+        schema_config*.table.each{ recentlyInsertedCache[it] = [] }
     }
-    
+
     def testConnection() {
         def result = 0;
         try {
@@ -26,14 +30,123 @@ class MariaDbConnector {
         } catch (e) {}
         3 == result ? 'OK' : 'Database connection could not be verified'
     }
-    
-    def read(table) {
-        sql.rows('select * from ' + table)
+
+    def read(table, whereColumns = [], whereValues = []) {
+        def whereClause = ''
+        if (whereColumns) {
+            whereClause = 'where ' + whereColumns.collect{ "$it = ?" }.join(' and ')
+        }
+        sql.rows("select * from $table $whereClause", whereValues)
     }
 
-    def insertData(data) {
-        //schema_config.each
+    def importBookData(data) {
+        data.each{ bookData ->
+            if (log_debug) { println "Processing entry $bookData" }
+            def tableIds = [:]
+            schema_config.each{ table ->
+                if (log_debug) { println "Collecting data for $table" }
+                // TODO: [test pending] Check if overlapping entry is already present
+                def columnNames = []
+                def columnData = []
+                table.columns.each{ columnSpec ->
+                    def name = columnSpec.name
+                    def datum
+                    if (name.toLowerCase().startsWith('id_')) {
+                        def referencedTable = name - 'id_' - 'ID_'
+                        datum = tableIds[referencedTable]
+                    } else {
+                        datum = grabPayloadData(columnSpec, bookData)
+                    }
+                    columnNames << name
+                    columnData << datum
+                }
 
+                def rowData = columnsToRows columnNames, columnData
+                rowData.each{ row ->
+                    persistSingleEntry table.table, columnNames, row, tableIds
+                }
+
+                if (log_debug) { println "Table done. Entry cache state: $tableIds" }
+            }
+        }
+    }
+
+    def columnsToRows(columnNames, columnData) {
+        if (columnData.every{ datum -> !( datum instanceof List ) }) {
+            return [columnData]
+        }
+        if ( !onlyIdColumnsAreNotLists(columnNames, columnData)
+            || !allListsAreSameSize(columnData)) {
+            throw new UnsupportedOperationException("Malconfigured table data: $columnNames $columnData")
+        }
+        def rowsNeeded = columnData.find{ it instanceof List }.size()
+        (0..rowsNeeded-1).collect{ i ->
+            def collected = []
+            columnNames.eachWithIndex{ name, j ->
+                collected << (name.toLowerCase().startsWith('id_') ? columnData[j] : columnData[j][i])
+            }
+            collected
+        }
+    }
+
+    def onlyIdColumnsAreNotLists(columnNames, columnData) {
+        def i = -1
+        columnNames.every{ name ->
+                i++
+                name.toLowerCase().startsWith('id_') || columnData[i] instanceof List
+        }
+    }
+
+    def allListsAreSameSize(columnData) {
+        1 == columnData.findAll{ it instanceof List }.collect{ it.size() }.unique().size()
+    }
+
+    def persistSingleEntry(tableName, columnNames, columnData, tableIds) {
+        insertData tableName, columnNames, columnData
+        def resultingDbEntries = read(tableName, columnNames, columnData)
+        assert 1 == resultingDbEntries.size()
+        def resultEntry = resultingDbEntries[0]
+        if (log_debug) { println "Found inserted entry: $resultEntry" }
+        rememberEntryIdForFutureReference tableName, resultEntry, tableIds
+    }
+
+    def grabPayloadData(columnSpec, data) {
+        columnSpec.with{
+            assert null != type
+            assert null != source
+            assert null != data[source]
+            switch(type.toLowerCase()) {
+                case ~/^int.*/:     return scalarOrListCast(data[source], Integer)
+                case ~/^bigint.*/:  return scalarOrListCast(data[source], BigInteger)
+                case ~/^varchar.*/: return scalarOrListCast(data[source], String)
+            }
+        }
+    }
+
+    def scalarOrListCast(item, clazz) {
+        (item instanceof List) ? item.collect{ it.asType(clazz) } : item.asType(clazz)
+    }
+
+    def insertData(table, columns, data) {
+        def n = columns.size()
+        assert n == data.size()
+        def query = "insert into $table (${columns.join(', ')}) values (${(['?']*n).join(', ')})"
+        if (log_debug) { println "Executing insert query: $query\nParameters: $data" }
+        sql.execute query, data
+    }
+
+    def rememberEntryIdForFutureReference(tableName, resultEntry, tableIds) {
+        def entryId
+        if (resultEntry.containsKey('id')) {
+            entryId = resultEntry.id
+        }
+        if (resultEntry.containsKey('ID')) {
+            entryId = resultEntry.ID
+        }
+        if (entryId) {
+            tableIds << [(tableName): entryId]
+            recentlyInsertedCache[tableName] << resultEntry // FUTURE: limit max cache size
+        }
     }
 
     def getSql() {
